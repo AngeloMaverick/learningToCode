b37006d5454e8cd5b12ba40cb672f6525d0a2a5c
AngeloMaverick <angelo.santos4@fatec.sp.gov.br>
2020-10-31 23:53:41 -0300

    A period of study

---
+++ javascript/javascriptDoBasicoAoAvancado/12_DebugsEErros/02_consolelog/script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let opa = 'teste';
+//delete Object.prototype; //(não é possivel apagar codigo nativo)
+
+function teste() {
+    "use strict"
+    let testando = 'teste';
+}
+
+teste();
+
+//false.prop = "";
+//"teste".prop = "";
