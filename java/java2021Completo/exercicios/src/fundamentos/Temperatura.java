6d2d922faf26e33406739b2654cd1fcb2f14a9f3
AngeloMaverick <angelo.santos4@fatec.sp.gov.br>
2021-03-15 14:18:29 -0300

    Até a Aula 26

--- java/java2021Completo/exercicios/src/fundamentos/Temperatura.java
+++ java/java2021Completo/exercicios/src/fundamentos/Temperatura.java
@@ -6,12 +6,17 @@ public class Temperatura {
 		//  VAR  CONST CONST VAR
 		final int ajuste = 32;
 		final double fator = 5/9.0;
-		double fah = 35;
-		double cel;
 		
+		double fah = 86;
+		double cel = (fah - ajuste)*fator;
+
 		System.out.println("("+ fah + "°F - 32) x 5/9 = °C");
-		//System.out.println(n2);
+		System.out.println(cel + "°C");
+
+		fah = 0;
 		cel = (fah - ajuste)*fator;
+
+		System.out.println("("+ fah + "°F - 32) x 5/9 = °C");
 		System.out.println(cel + "°C");
 	}
 }
