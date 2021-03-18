67d14cfa5b12ecea1a391c514831988736305f5c
Angelo Santos <angelo.santos4@fatec.sp.gov.br>
2021-03-18 16:45:48 -0300

    Aula 53 Finalizada

---
+++ java/java2021Completo/exercicios/src/fundamentos/TipoStringEquals.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class TipoStringEquals {
+	
+	public static void main(String[] args) {
+		System.out.println("2" == "2"); //true
+
+		String s1 = new String("2"); 
+		System.out.println("2" == s1); //false
+		System.out.println("2".equals(s1)); //true
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String s2 = entrada.nextLine();
+		System.out.println("2" == s2.trim()); //trim tira os espaços em branco
+		System.out.println("2".equals(s2.trim()));
+		// O "entrada.next()" ja retira os espaços em branco sem necessidade do trim
+		//Comparação de Strings deve ser feita com ".equals()"
+		
+		entrada.close();
+	}
+}
---
+++ java/java2021Completo/exercicios/src/fundamentos/exercicios/Exercicio01.java
@@ -0,0 +1,10 @@
+package fundamentos.exercicios;
+
+public class Exercicio01 {
+	
+	public static void main(String[] args) {
+		double celsius = 16;
+		double fahrenheit = (1.8 * celsius) + 32;
+		System.out.println(fahrenheit + "F°");
+	}
+}
