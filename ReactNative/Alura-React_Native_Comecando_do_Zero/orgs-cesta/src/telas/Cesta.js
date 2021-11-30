513583e652b5ce84c9bc2b77b5098a89d7380529
Angelo Santos <angelo.santos4@fatec.sp.gov.br>
2021-11-29 22:39:05 -0300

    commit society challenge | Day 8

--- ReactNative/Alura-React_Native_Comecando_do_Zero/orgs-cesta/src/telas/Cesta.js
+++ ReactNative/Alura-React_Native_Comecando_do_Zero/orgs-cesta/src/telas/Cesta.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Image, StyleSheet, Dimensions, Text, View } from 'react-native';
 
+import Texto from './componentes/Texto';
+
 import topo from '../../assets/topo.png';
 import logo from '../../assets/logo.png';
 
@@ -10,20 +12,20 @@ const width = Dimensions.get('screen').width;
 export default function Cesta() {
     return <>
         <Image source={topo} style={estilos.topo} />
-        <Text style={estilos.titulo}>Detalhe da cesta</Text>
+        <Texto style={estilos.titulo}>Detalhe da cesta</Texto>
 
         <View style={estilos.cesta}>
-            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <Texto style={estilos.nome}>Cesta de Verduras</Texto>
             <View style={estilos.fazenda}>
                 <Image source={logo} style={estilos.imagemFazenda} />
-                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+                <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
             </View>
-            <Text style={estilos.descricao}>
+            <Texto style={estilos.descricao}>
                 Uma cesta com produtos selecionados
                 cuidadosamente da fazendo direto para
                 a cozinha
-            </Text>
-            <Text style={estilos.preco}>R$ 40,00</Text>
+            </Texto>
+            <Texto style={estilos.preco}>R$ 40,00</Texto>
         </View>
     </>
 }
@@ -64,7 +66,7 @@ const estilos = StyleSheet.create({
     nomeFazenda: {
         fontSize: 16,
         lineHeight: 26,
-        marginLeft: 12
+        marginLeft: 12,
     },
     descricao: {
         color: "#A3A3A3",
