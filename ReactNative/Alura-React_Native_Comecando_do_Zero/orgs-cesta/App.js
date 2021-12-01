71458a056e36cbc90d8dde77f564dc5a590dff6d
Angelo Santos <angelo.santos4@fatec.sp.gov.br>
2021-12-01 08:07:24 -0300

    commit society challenge | Day 10

--- ReactNative/Alura-React_Native_Comecando_do_Zero/orgs-cesta/App.js
+++ ReactNative/Alura-React_Native_Comecando_do_Zero/orgs-cesta/App.js
@@ -6,7 +6,8 @@ import {
   Montserrat_700Bold
 } from '@expo-google-fonts/montserrat';
 
-import Cesta from './src/telas/Cesta';
+import Cesta from './src/screens/Cesta';
+import mock from './src/mocks/cesta';
 
 export default function App() {
   const [fonteCarregada] = useFonts({
@@ -21,7 +22,7 @@ export default function App() {
   return (
     <SafeAreaView>
       <StatusBar />
-      <Cesta />
+      <Cesta {...mock} />
     </SafeAreaView>
   );
 }
