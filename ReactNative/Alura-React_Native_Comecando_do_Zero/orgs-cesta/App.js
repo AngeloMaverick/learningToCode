8d7717ba07833fa1a8fd3af6e4d00145306c1e02
Angelo Santos <angelo.santos4@fatec.sp.gov.br>
2021-12-02 23:29:27 -0300

    commit society challenge | Day 11

--- ReactNative/Alura-React_Native_Comecando_do_Zero/orgs-cesta/App.js
+++ ReactNative/Alura-React_Native_Comecando_do_Zero/orgs-cesta/App.js
@@ -5,6 +5,7 @@ import {
   Montserrat_400Regular,
   Montserrat_700Bold
 } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
 
 import Cesta from './src/screens/Cesta';
 import mock from './src/mocks/cesta';
@@ -16,7 +17,7 @@ export default function App() {
   })
 
   if (!fonteCarregada) {
-    return <View />
+    return <AppLoading />
   }
 
   return (
