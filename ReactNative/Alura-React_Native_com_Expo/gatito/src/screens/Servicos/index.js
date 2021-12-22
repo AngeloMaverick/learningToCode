fdebf53d0acb76084f7877410814a9ce99c3e68a
Angelo Santos <angelo.santos4@fatec.sp.gov.br>
2021-12-21 23:52:29 -0300

    commit society challenge | Day 30

--- ReactNative/Alura-React_Native_com_Expo/gatito/src/screens/Servicos/index.js
+++ ReactNative/Alura-React_Native_com_Expo/gatito/src/screens/Servicos/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { SafeAreaView, StatusBar, FlatList } from 'react-native';
+import { SafeAreaView, StatusBar, FlatList, KeyboardAvoidingView, Platform } from 'react-native';
 import Item from './Item';
+import estilosGlobal from '../../estilos';
 
 const servicos = [
     {
@@ -25,13 +26,18 @@ const servicos = [
 ];
 
 export default function Servicos() {
-    return <SafeAreaView>
+    return <SafeAreaView style={estilosGlobal.preencher}>
         <StatusBar />
+        {/*<KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+            style={estilosGlobal.preencher}
+        >*/}
         <FlatList
             data={servicos}
             removeClippedSubviews={false}
             renderItem={({ item }) => <Item {...item} />}
             keyExtractor={({ id }) => String(id)}
         />
+        {/*</KeyboardAvoidingView>*/}
     </SafeAreaView>
 }
