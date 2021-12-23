7c4fc7d4357d2a0a655874b0364f25061b5d91fc
Angelo Santos <angelo.santos4@fatec.sp.gov.br>
2021-12-22 23:36:10 -0300

    commit society challenge | Day 31

--- ReactNative/Alura-React_Native_com_Expo/gatito/App.js
+++ ReactNative/Alura-React_Native_com_Expo/gatito/App.js
@@ -3,7 +3,8 @@ import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 
 import Servicos from './src/screens/Servicos';
+import Carrinho from './src/screens/Carrinho';
 
 export default function App() {
-  return <Servicos />;
+  return <Carrinho />;
 }
