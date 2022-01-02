6af51bd889d455e5abe9045d3ebf31dd17dc7d65
Angelo Santos <angelo.santos4@fatec.sp.gov.br>
2022-01-01 23:55:18 -0300

    commit society challenge | Day 41

--- ReactNative/Alura-React_Native_com_Expo/gatito/App.js
+++ ReactNative/Alura-React_Native_com_Expo/gatito/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import Reactotron from 'reactotron-react-native';
 
 import TelaPadrao from './src/components/TelaPadrao';
 import Rotas from './src/Rotas';
 
+Reactotron.configure().useReactNative().connect();
+
 export default function App() {
+  Reactotron.log("Alura");
+
   return <TelaPadrao><Rotas /></TelaPadrao>;
 }
--- ReactNative/Alura-React_Native_com_Expo/gatito/src/components/TelaPadrao/index.js
+++ ReactNative/Alura-React_Native_com_Expo/gatito/src/components/TelaPadrao/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import estilosGlobal from '../../estilos';
+import estilosGlobal, { cores } from '../../estilos';
+import estilos from './estilos';
 
 export default function TelaPadrao({ children }) {
-    return <SafeAreaView style={estilosGlobal.preencher}>
-        <StatusBar />
-        {/*<KeyboardAvoidingView
+    return <>
+        <SafeAreaView style={estilos.ajusteTela}>
+            <StatusBar backgroundColor={cores.roxo} />
+            {/*<KeyboardAvoidingView
         behavior={Platform.OS == "ios" ? "padding" : "height"}
         style={estilosGlobal.preencher}
     >*/}
-        {children}
-        {/*</KeyboardAvoidingView>*/}
-    </SafeAreaView>
+            {children}
+            {/*</KeyboardAvoidingView>*/}
+        </SafeAreaView>
+        {/*StatusBar de baixo, caso visível no celular utilizado. */}
+        <SafeAreaView style={estilos.ajusteTelaBaixo} />
+    </>
 }
