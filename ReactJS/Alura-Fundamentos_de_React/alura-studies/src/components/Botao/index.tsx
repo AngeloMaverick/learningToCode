95750ca4474721fbaa0fa57f6abf1e2b26cbb271
Angelo Santos <angelo.santos4@fatec.sp.gov.br>
2022-01-26 23:51:08 -0300

    commit society challenge | Day 66

--- ReactJS/Alura-Fundamentos_de_React/alura-studies/src/components/Botao/index.tsx
+++ ReactJS/Alura-Fundamentos_de_React/alura-studies/src/components/Botao/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{
+interface Props {
+  type?: "button" | "submit" | "reset" | undefined,
+  onClick?: () => void,
+  children?: React.ReactNode
+}
+
+function Botao({onClick, type, children}: Props) {
+  return (
+    <button 
+      onClick={onClick} 
+      type={type} 
+      className={style.botao}
+    >
+      {children}
+    </button>
+  )
+}
+
+/*
+class Botao1 extends React.Component<{
   type?: "button" | "submit" | "reset" | undefined,
   onClick?: () => void
 }> {
@@ -14,5 +33,6 @@ class Botao extends React.Component<{
     );
   }
 }
+*/
 
 export default Botao;
